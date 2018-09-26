c4611ae24a41e24dabe4c927e85dcc38614ef21e
bennitalg <bennitalg@outlook.com>
2018-09-26 09:29:14 -0500

    4-1 Loop

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -6,7 +6,22 @@ namespace Loop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Create a program that asks the user to enter a number. 
+             * Loop from 2 to 20 and multiply the loop value by the number the user 
+             * entered and output the value each time.  When the loop is finished, output the sum of all previous values. 
+             */
+
+            double result = 0;
+
+
+            for (double i = 2; i <= 20; i++)
+            {
+                Console.Write("Please enter a number: ");
+                result = result * i;
+                result += Convert.ToDouble(Console.ReadLine());
+            }
+            Console.Write("The sum is: " + result);
+            Console.ReadLine();
         }
     }
 }
